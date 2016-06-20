c5e0fe6073fcf6a485ae7f42ddc4438efb470f72
hidepy <hidork0222@yahoo.co.jp>
2016-06-20 18:32:06 +0900

    2016/06/20 開発再開

--- www/js/app.ts
+++ www/js/app.ts
@@ -1,61 +1,89 @@
+declare var myNavigator: NavigatorView;
+declare var myTabbar: TabbarView;
 /// <reference path="./storageManager.ts" />
 /// <reference path="./commonFunctions.ts" />
-(function () {
+
+
+(function(){
     'use strict';
-    var storage_manager = new StorageManager("WIKI_DIVER_FAVORITE");
+
+    var storage_manager: StorageManager = new StorageManager("WIKI_DIVER_FAVORITE");
     var module = angular.module('app', ['onsen']);
-    module.controller("RootController", function ($scope) {
-        $scope.sharing = {};
-        $scope.sharing["hide_tabbar"] = false;
-        //$scope.direct = "I'm direct prop"; //これもok
+
+    module.controller("RootController", function($scope){
+      $scope.sharing = {};
+      $scope.sharing["hide_tabbar"] = false;
+
+      //$scope.direct = "I'm direct prop"; //これもok
     });
-    module.controller('MasterController', function ($scope, $data) {
+
+
+    module.controller('MasterController', function($scope, $data) {
         $scope.items = $data.items;
-        $scope.showDetail = function (index) {
+
+        $scope.showDetail = function(index) {
             console.log("show detail comes");
             var selectedItem = $data.items[index];
             $data.selectedItem = selectedItem;
             //$scope.ons.navigator.pushPage('detail.html', {title : selectedItem.title});
-            myNavigator.pushPage('entry_record.html', 
-            //{title : selectedItem.title}
-            {
-                onTransitionEnd: {
-                    title: selectedItem.title
+            myNavigator.pushPage(
+              'entry_record.html',
+              //{title : selectedItem.title}
+              {
+                onTransitionEnd:{
+                  title: selectedItem.title
                 }
-            });
+              }
+            );
         };
     });
-    module.controller("HomeController", function ($scope, currentBikeInfo) {
+
+
+    module.controller("HomeController", function($scope, currentBikeInfo){
         $scope.data = currentBikeInfo;
+
         $scope.visibility = {};
         $scope.visibility.dbg_disp_area = "inline";
+
         console.log("tabbar object is=");
         console.log(myTabbar);
+
         // tabbar可視性
         $scope.sharing.hide_tabbar = false;
-        $scope.movetoViewRecordHeader = function () {
+
+        $scope.movetoViewRecordHeader = function(){
             console.log("in movetoViewRecordHeader");
             myNavigator.pushPage("view_record_header_page.html");
         };
     });
-    module.controller('EntryController', function ($scope, selectList) {
+
+
+    module.controller('EntryController', function($scope, selectList) {
+
         console.log("in EntryController");
+
         $scope.bike = "";
-        $scope.d_bunrui = "";
+        $scope.d_bunrui =  "";
         $scope.c_bunrui = "";
         $scope.title = "";
         $scope.odd_meter = "";
         $scope.date = "";
         $scope.money = "";
         $scope.comment = "";
+
         $scope.UPDATE_BUTTON_NAME = "登録";
+
         // tabbar可視性
         $scope.sharing.hide_tabbar = true;
         console.log("tabbar 可視性=" + $scope.sharing.hide_tabbar);
+
+
         var args = myNavigator.getCurrentPage().options;
+
         //詳細ページから編集を押下してきた場合
-        if (args.is_modify) {
+        if(args.is_modify){
             var item = args.item;
+
             //詳細情報をコピー
             $scope.bike = item.bike;
             $scope.d_bunrui = item.d_bunrui;
@@ -65,13 +93,19 @@
             $scope.date = item.date;
             $scope.money = item.money;
             $scope.comment = item.comment;
+
             $scope.UPDATE_BUTTON_NAME = "更新";
         }
+
+
         //登録処理
-        $scope.processEntryRecord = function () {
+        $scope.processEntryRecord = function(){
             //ここでエラー判定処理
+
             var id = formatDate(new Date());
+
             var msg = "";
+
             var record = {
                 id: id,
                 bike: $scope.bike,
@@ -83,55 +117,80 @@
                 money: $scope.money,
                 comment: $scope.comment
             };
+
             //レコードを1件格納
-            if (storage_manager.saveItem2Storage(id, record)) {
+            if(storage_manager.saveItem2Storage(id, record)){
                 msg = "success to save!!";
+
                 myNavigator.popPage(); //前画面へ
             }
-            else {
+            else{
                 msg = "failed to save....";
             }
+
             showAlert(msg);
-        };
-        $scope.showSelectListBike = function () {
+        }
+
+        $scope.showSelectListBike = function(){
+
             selectList.removeAllItems();
+
             selectList.addItem("1", "GN125");
             selectList.addItem("2", "VT250");
             selectList.addItem("3", "SRX250");
             selectList.addItem("4", "VTR250");
-            myNavigator.pushPage('list_select_page.html', 
-            //{title: "bike"}
-            {
-                onTransitionEnd: {
-                    title: "bike"
-                }
-            });
+
+            myNavigator.pushPage(
+              'list_select_page.html',
+               //{title: "bike"}
+               {
+                 onTransitionEnd: {
+                   title: "bike"
+                 }
+               }
+            );
         };
-        $scope.showSelectListDBunrui = function () {
+
+        $scope.showSelectListDBunrui = function(){
+
             selectList.removeAllItems();
+
             selectList.createItemsFromArr(["メンテナンス", "カスタム", "燃費"]);
-            myNavigator.pushPage("list_select_page.html", 
-            //{title: "d_bunrui"}
-            {
-                onTransitionEnd: {
-                    title: "d_bunrui"
+
+            myNavigator.pushPage(
+              "list_select_page.html",
+              //{title: "d_bunrui"}
+              {
+                onTransitionEnd:{
+                  title: "d_bunrui"
                 }
-            });
+              }
+            );
         };
-        $scope.showSelectListCBunrui = function () {
+
+        $scope.showSelectListCBunrui = function(){
+
             selectList.removeAllItems();
+
             selectList.createItemsFromArr(["オイル交換", "フォークオイル交換", "ブレーキフルード交換"]);
-            myNavigator.pushPage("list_select_page.html", 
-            //{title: "c_bunrui"}
-            {
-                onTransitionEnd: {
-                    title: "c_bunrui"
-                }
-            });
+
+            myNavigator.pushPage(
+              "list_select_page.html",
+               //{title: "c_bunrui"}
+               {
+                 onTransitionEnd:{
+                   title: "c_bunrui"
+                 }
+               }
+             );
+
         };
-        $scope.$on("listSelected", function (e, param) {
+
+        $scope.$on("listSelected", function(e, param){
+
             //$scope.selected_bike = item.value;
-            switch (param.parent_option.title) {
+
+            switch(param.parent_option.title){
                 case "bike":
                     $scope.bike = param.item.value;
                     break;
@@ -145,112 +204,164 @@
                     console.log("return value missing...");
             }
         });
+
     });
-    module.controller("SelectListController", function ($scope, $rootScope, selectList) {
+
+    module.controller("SelectListController", function($scope, $rootScope, selectList){
+
         $scope.items = selectList.items;
-        $scope.processItemSelect = function (index) {
+
+        $scope.processItemSelect = function(index){
             var nav_options = myNavigator.getCurrentPage().options;
             var selectedItem = selectList.items[index];
             selectList.selectedItem = selectedItem;
             myNavigator.popPage();
+
             // イベント通知
-            $rootScope.$broadcast("listSelected", { parent_option: nav_options, item: selectedItem });
-        };
+            $rootScope.$broadcast("listSelected", {parent_option: nav_options, item: selectedItem});
+
+        }
     });
-    module.factory("currentBikeInfo", function () {
-        var data = {};
+
+    module.factory("currentBikeInfo", function(){
+        var data: any = {};
+
         data.name = "gn125";
         data.purchace_date = "2012/03/11";
         data.comment = "this is my first bike";
         data.img = "none";
         data.maintainance_records = 11;
         data.touring_records = 21;
+
         return data;
+
     });
-    module.service("selectList", function () {
+
+    module.service("selectList", function(){
         this.items = [];
         this.selectedItem = {};
-        this.addItem = function (_key, _value) {
+        this.addItem = function(_key, _value){
             this.items.push({
                 key: _key,
                 value: _value
             });
         };
-        this.removeItem = function (idx) {
+        this.removeItem = function(idx){
             this.items.splice(idx, 1);
         };
-        this.removeAllItems = function () {
+        this.removeAllItems = function(){
             this.items.length = 0;
         };
-        this.createItemsFromObjectArr = function (objArr, key_name, value_name) {
+        this.createItemsFromObjectArr = function(objArr, key_name, value_name){
             /*
             objArr.forEach(function(val, idx, objArr){
                 this.addItem(val[key_name], val[value_name]);
             });
             */
-            for (var i = 0; i < objArr.length; i++) {
+            for(var i = 0; i < objArr.length; i++){
                 this.addItem(objArr[i][key_name], objArr[i][value_name]);
             }
+
         };
-        this.createItemsFromArr = function (arr) {
+        this.createItemsFromArr = function(arr){
             /*
             arr.forEach(function(val, idx){
                 this.addItem(idx, val);
             });
             */
-            for (var i = 0; i < arr.length; i++) {
+            for(var i = 0; i < arr.length; i++){
                 this.addItem("" + i, arr[i]);
             }
         };
+
     });
-    module.controller("_ts", function (currentBikeInfo) {
+
+    module.controller("_ts", function(currentBikeInfo){
         this.data = currentBikeInfo;
     });
-    module.controller("ViewRecordHeaderController", function ($scope) {
+
+
+    module.controller("ViewRecordHeaderController", function($scope){
+
         //整備情報レコード
         $scope.items = convHash2Arr(storage_manager.getAllItem());
+
         console.log("in ViewRecordHeaderController");
         console.log($scope.items);
-        $scope.processItemSelect = function (index, event) {
+
+        $scope.processItemSelect = function(index, event){
             console.log("item tapped!! send item is:");
+
             console.log($scope.items[index]);
+
             console.log("event is:");
             console.log(event);
+
             console.log("index is: " + index);
-            myNavigator.pushPage('view_record_detail_page.html', 
-            //{item : $scope.items[index]}
-            {
-                onTransitionEnd: {
-                    item: $scope.items[index]
+
+            myNavigator.pushPage(
+              'view_record_detail_page.html',
+              //{item : $scope.items[index]}
+              {
+                onTransitionEnd:{
+                  item: $scope.items[index]
                 }
-            });
+              }
+            );
+
         };
     });
-    module.controller("ViewRecordDetailConrtoller", function ($scope) {
+
+
+    module.controller("ViewRecordDetailConrtoller", function($scope){
+
         console.log("in ViewRecordDetailConrtoller");
+
         var args = myNavigator.getCurrentPage().options;
+
         $scope.item = args.item; //整備情報を取得(前画面からの情報まんまでよいか？)
+
         console.log($scope.item);
-        $scope.movetoUpdate = function () {
+
+        $scope.movetoUpdate = function(){
             console.log("item tapped!!");
-            myNavigator.pushPage('entry_record.html', 
-            //{item : $scope.item, is_modify: true}
-            {
-                onTransitionEnd: {
-                    item: $scope.item,
-                    is_modify: true
+
+            myNavigator.pushPage(
+              'entry_record.html',
+              //{item : $scope.item, is_modify: true}
+              {
+                onTransitionEnd:{
+                  item: $scope.item,
+                  is_modify: true
                 }
-            });
+              }
+            );
+
         };
-        $scope.processItemDelete = function () {
+
+        $scope.processItemDelete = function(){
             console.log("ここで削除時の動作をしますよ！");
         };
+
+
     });
-    module.controller('DetailController', function ($scope, $data) {
+
+
+
+
+
+
+
+
+
+
+    module.controller('DetailController', function($scope, $data) {
         $scope.item = $data.selectedItem;
     });
-    module.factory('$data', function () {
-        var data = {};
+
+    module.factory('$data', function() {
+        var data: any = {};
+
         data.items = [
             {
                 title: 'Item 1 Title',
@@ -273,6 +384,15 @@
                 desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
             }
         ];
+
         return data;
     });
+
+
+
+
+
+
+
+
 })();
--- www/js/storageManager.js
+++ www/js/storageManager.js
@@ -1,80 +1,94 @@
+/*********************************
 
-var StorageManager = function(){
+    localStorage manager
 
-	console.log("StorageManager initialize start");
-
-	this.MAINTAINANCE_RECORD_NAME = "MAINTAINANCE_RECORDS"; //整備情報
-
-	this.maintainance_records = this.convStorage2Hash(); //整備情報を全件取得
-
-	console.log("StorageManager initialize end");
-
-};
-
-/* まあ、シングルトンオブジェクトなんで、prototypeしなくていいんだけどね */
-
-
-/* local storageからオブジェクトを生成 */
-StorageManager.prototype.convStorage2Hash = function(){
-	var item_hash = JSON.parse(window.localStorage.getItem(this.MAINTAINANCE_RECORD_NAME));
-
-	if(!item_hash){
-		item_hash = {};
-	}
-
-	return item_hash;
-}
-/* 全件取得 */
-StorageManager.prototype.getAllItem = function(){
-	return this.maintainance_records;
-}
-StorageManager.prototype.getAllItemArray = function(){
-	return convHash2Arr(this.maintainance_records);
-}
-/* とある1件のみ取得 */
-StorageManager.prototype.getDetailItem = function(key){
-	return this.maintainance_records[key];
-}
-/* 1件削除 */
-StorageManager.prototype.deleteItem = function(key){
-
-	//ハッシュから削除
-	delete　this.maintainance_records[key];
-
-	//ストレージにセット
-	window.localStorage.setItem(this.MAINTAINANCE_RECORD_NAME, JSON.stringify(this.maintainance_records));
-}
-/* 商品情報1件をハッシュに保存 */
-StorageManager.prototype.saveItem = function(key, data){
-
-	try{
-		this.maintainance_records[key] = data;
-
-		window.localStorage.setItem(this.MAINTAINANCE_RECORD_NAME, JSON.stringify(this.maintainance_records));
-
-		return true;
-	}
-	catch(e){
-		console.log("on saveItem, error occured...");
-	}
-
-	return false;
-
-}
-
-
+**********************************/
 /*
-		var data = {
-			id
-			bike
-			d_bunrui
-			c_bunrui
-			title
-			odd_meter
-			date
-			money
-			comment
-		};
+**********	properties	**********
+なし
+
+
+**********	methods		**********
+convStorage2Hash -private
+    _storage_key_nameをキーとし、localstorageから保存されたデータを取得する
+
+getAllItem -public
+    現在保持しているハッシュの値を全件返却する
+
+getItem(key) -public
+    key に指定された値をキーとして、現在保持しているハッシュの値1件を返却する
+
+getItemLength　-public
+    保存されている要素の数を返却する
+
+deleteItem(key) -public
+    key に指定された値をキーとして、現在保持しているハッシュ、localstorageから1件削除する
+
+deleteItems(keys) -public
+    keys(文字配列) に指定された要素を、現在保持しているハッシュ、localstorageから削除する
+
+saveItem2Storage(key, data) -public
+    key に指定された値をキーとし、dataを現在保持しているハッシュ、localstorageに1件登録する
+
 */
-
-
+var StorageManager = function (storage_key_name) {
+    this.storage_key_name = storage_key_name;
+    this.init(storage_key_name);
+};
+// プロトタイプ定義
+!function (proto) {
+    var _storage_key_name = "";
+    var _items = {};
+    var _ref_arr = [];
+    proto.init = function (storage_key_name) {
+        //initialize...
+        //対象となるlocalstorageのキーを格納
+        _storage_key_name = storage_key_name;
+        //インスタンス化直後、現在のストレージの情報をハッシュに格納する
+        _items = convStorage2Hash(storage_key_name);
+    };
+    var convStorage2Hash = function (storage_key_name) {
+        var item_hash = {};
+        try {
+            var s = window.localStorage.getItem(storage_key_name);
+            if (s) {
+                item_hash = JSON.parse(s);
+            }
+        }
+        catch (e) {
+            console.log("convStorage2Hash eror occured");
+        }
+        return item_hash;
+    };
+    proto.getAllItem = function () {
+        return _items;
+    };
+    proto.getItem = function (key) {
+        return _items[key];
+    };
+    proto.deleteItem = function (key) {
+        delete _items[key];
+        window.localStorage.setItem(this.storage_key_name, JSON.stringify(_items));
+    };
+    proto.deleteItems = function (keys) {
+        if (keys && keys.length && (keys.length > 0)) {
+            keys.forEach(function (v, i, arr) {
+                delete _items[v];
+            });
+        }
+        window.localStorage.setItem(_storage_key_name, JSON.stringify(_items));
+    };
+    proto.saveItem2Storage = function (key, data) {
+        _items[key] = data;
+        window.localStorage.setItem(_storage_key_name, JSON.stringify(_items));
+    };
+    proto.sortByKey = function (key, desc) {
+    };
+    proto.getItemLength = function () {
+        return Object.keys(_items).length;
+    };
+    // ！！！！！！！！！！通常使用不可！！！！！！！！！！
+    proto.deleteAllItem = function () {
+        window.localStorage.setItem(_storage_key_name, JSON.stringify({}));
+    };
+}(StorageManager.prototype);
